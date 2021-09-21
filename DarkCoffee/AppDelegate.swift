c8facd6c69465b3703eacc75c95c8dcd7de7fa1d
Steven J. Selcuk <stevenjselcuk@gmail.com>
2021-09-21 20:22:56 +0300

    Initial Commit

---
+++ DarkCoffee/AppDelegate.swift
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.swift
+//  DarkCoffee
+//
+//  Created by Steven J. Selcuk on 21.09.2021.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    func applicationDidFinishLaunching(_ notification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ notification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+    func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+        return true
+    }
+
+}
